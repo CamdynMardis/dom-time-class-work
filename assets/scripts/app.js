d0317f9c8a2670c952d6e9d694a78f31f32b1e02
Camdyn Mardis <319200523+CamdynMardis@users.noreply.github.com>
2026-09-10 10:34:43 -0500

    Refactor app.js to initialize DOM elements and add listeners

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,24 +1,73 @@
-const helloBtn = document.querySelector("#hello-btn");
-const helloBox = document.querySelector("#hello-box");
-const userGreeting = document.querySelector("#user-greeting");
-const terminal = document.querySelector("#terminal");
+let helloBtn = "";
+let helloBox = "";
+let userGreeting = "";
+let terminal = "";
 
-console.log("whats in hellBox variable", helloBox);
+//const is an unchanging variable, while let allows a varbiable to change.
 
-helloBtn.addEventListener("click", () => {
-  console.log("hellobx valueu", helloBox.value);
-  // alert("hellodsfasdfsa!");
-  userGreeting.textContent = "hi there, " + helloBox.value + "!";
+document.addEventListener("DOMContentLoaded", () => {
+  //console.log("DOM ready!");
 
-  const line = document.createElement("p");
-
-  line.textContent = helloBox.value;
-
-  terminal.appendChild(line);
+  initDomStuff();
+  addListeners();
 });
+
+function initDomStuff() {
+  // grab all dom stuff in vars
+  helloBtn = document.querySelector("#hello-btn");
+  helloBox = document.querySelector("#hello-box");
+  userGreeting = document.querySelector("#user-greeting");
+  terminal = document.querySelector("#terminal");
+  console.log("dom variables initialized. godspeed. ");
+}
+
+function addListeners() {
+  //console.log("whats in hellBox variable", helloBox);
+
+  helloBtn.addEventListener("click", () => {
+
+    sessionStorage.setItem("userName", helloBox.value);
+
+    console.log("hellobx valueu", helloBox.value);
+
+    console.log("hellobx value from session", sessionStorage.getItem("userName"));
+    // alert("hellodsfasdfsa!");
+    userGreeting.textContent = "hi there, " + helloBox.value + "!";
+
+    const line = document.createElement("p");
+
+    line.textContent = helloBox.value;
+
+    terminal.appendChild(line);
+  });
+}
+
+function functionName(userName, income) {
+
+  let sum = 2 + 2;
+  console.log('sum is, ', sum);
+
+  return sum;
+}
+
+() => {return 4;}
+
+// console.log("whats in hellBox variable", helloBox);
+
+// helloBtn.addEventListener("click", () => {
+//   console.log("hellobx valueu", helloBox.value);
+//   // alert("hellodsfasdfsa!");
+//   userGreeting.textContent = "hi there, " + helloBox.value + "!";
+
+//   const line = document.createElement("p");
+
+//   line.textContent = helloBox.value;
+
+//   terminal.appendChild(line);
+// });
 
 // document.getElementById("hello-btn").addEventListener('click',
 
-// 	function(){alert('hello there')}
+// function(){alert('hello there')}
 
 // )
