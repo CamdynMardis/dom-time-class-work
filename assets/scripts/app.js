6a169d5f90df693b3bf7f435d8d6f60ff3df66cf
Camdyn Mardis <319200523+CamdynMardis@users.noreply.github.com>
2026-09-18 20:02:07 -0500

    Refactor app.js to improve variable declarations
    Refactor app.js to use const instead of let for DOM elements and streamline event listener setup.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,73 +1,24 @@
-let helloBtn = "";
-let helloBox = "";
-let userGreeting = "";
-let terminal = "";
+const helloBtn = document.querySelector("#hello-btn");
+const helloBox = document.querySelector("#hello-box");
+const userGreeting = document.querySelector("#user-greeting");
+const terminal = document.querySelector("#terminal");
 
-//const is an unchanging variable, while let allows a varbiable to change.
+console.log("whats in hellBox variable", helloBox);
 
-document.addEventListener("DOMContentLoaded", () => {
-  //console.log("DOM ready!");
+helloBtn.addEventListener("click", () => {
+  console.log("hellobx valueu", helloBox.value);
+  // alert("hellodsfasdfsa!");
+  userGreeting.textContent = "hi there, " + helloBox.value + "!";
 
-  initDomStuff();
-  addListeners();
+  const line = document.createElement("p");
+
+  line.textContent = helloBox.value;
+
+  terminal.appendChild(line);
 });
-
-function initDomStuff() {
-  // grab all dom stuff in vars
-  helloBtn = document.querySelector("#hello-btn");
-  helloBox = document.querySelector("#hello-box");
-  userGreeting = document.querySelector("#user-greeting");
-  terminal = document.querySelector("#terminal");
-  console.log("dom variables initialized. godspeed. ");
-}
-
-function addListeners() {
-  //console.log("whats in hellBox variable", helloBox);
-
-  helloBtn.addEventListener("click", () => {
-
-    sessionStorage.setItem("userName", helloBox.value);
-
-    console.log("hellobx valueu", helloBox.value);
-
-    console.log("hellobx value from session", sessionStorage.getItem("userName"));
-    // alert("hellodsfasdfsa!");
-    userGreeting.textContent = "hi there, " + helloBox.value + "!";
-
-    const line = document.createElement("p");
-
-    line.textContent = helloBox.value;
-
-    terminal.appendChild(line);
-  });
-}
-
-function functionName(userName, income) {
-
-  let sum = 2 + 2;
-  console.log('sum is, ', sum);
-
-  return sum;
-}
-
-() => {return 4;}
-
-// console.log("whats in hellBox variable", helloBox);
-
-// helloBtn.addEventListener("click", () => {
-//   console.log("hellobx valueu", helloBox.value);
-//   // alert("hellodsfasdfsa!");
-//   userGreeting.textContent = "hi there, " + helloBox.value + "!";
-
-//   const line = document.createElement("p");
-
-//   line.textContent = helloBox.value;
-
-//   terminal.appendChild(line);
-// });
 
 // document.getElementById("hello-btn").addEventListener('click',
 
-// function(){alert('hello there')}
+// 	function(){alert('hello there')}
 
 // )
